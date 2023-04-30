9704a485132f954f8f23956fbf0d83bbad591a5f
Ahmed abdel hamid <ahmedsalem1041998@gmail.com>
2023-04-30 12:54:43 +0300

    CreatePopUps

---
+++ DataMapping/Migrations/202304300931194_addSubjecttabel.cs
@@ -0,0 +1,28 @@
+﻿namespace DataMapping.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addSubjecttabel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "Gl.Subject",
+                c => new
+                    {
+                        SubjectId = c.Int(nullable: false, identity: true),
+                        SubjectCode = c.Int(),
+                        SubjectName = c.String(),
+                        IsDeleted = c.Boolean(),
+                    })
+                .PrimaryKey(t => t.SubjectId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("Gl.Subject");
+        }
+    }
+}
