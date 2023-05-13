886d698421d8c1e0439c27b9ad471e65e6388026
Ahmed Khaled <ahmed.java97@gmail.com>
2023-05-13 19:16:08 +0200

    GetClientsInfoProcedure

---
+++ DataMapping/Procedures/GetClientsInfo_Result.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMapping.Procedures
+{
+   public class GetClientsInfo_Result
+    {
+        public int ClientId { get; set; }
+
+      
+        public string CName { get; set; }
+
+
+        public string CTel1 { get; set; }
+
+        public string CTel2 { get; set; }
+
+        public string CAddres { get; set; }
+
+
+        public string CNotes { get; set; }
+        public int? CProgramID { get; set; }
+        public int? CProgramType { get; set; }
+
+    }
+}
